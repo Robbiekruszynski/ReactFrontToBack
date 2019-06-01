37eb978bfab7a32d93a6d46dcd3d2ff4cca550b8
Robbie Kruszynski <robbiekruszynski@gmail.com>
2019-06-01 15:46:04 -0700

    add work showing the spread and filter

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ let test;
 
 name = "Jack";
 
-const person = {
+const personExample = {
   name: "John",
   age: 33
 };
 
-person.name = "Jack";
-console.log(person);
+personExample.name = "Jack";
+console.log(personExample);
 
 const nums = [1, 2, 3, 4];
 nums.push(5);
@@ -55,3 +55,30 @@ const people = [
 
 const people2 = people.filter(person => person.id !== 2);
 console.log(people2);
+
+//SPREAD
+
+const arr = [1, 2, 3];
+const arr2 = [...arr, 4];
+const arr3 = [...arr.filter(num => num !== 2)];
+console.log(arr3);
+
+const person1 = {
+  name: "Robbie",
+  age: 30
+};
+
+const person2 = {
+  ...person1,
+  email: "robbie@gmail.com"
+};
+
+console.log(person2);
+
+//DESTRUCTURING
+
+//CLASSES
+
+//SUBCLASSES
+
+//MODULES
