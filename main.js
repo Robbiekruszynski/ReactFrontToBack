f384f007766f4d6a37b3caca58d6976cbcca26a3
Robbie Kruszynski <robbiekruszynski@gmail.com>
2019-06-03 09:41:12 -0700

    add final work to ES6 refresh

--- main.js
+++ main.js
@@ -98,6 +98,47 @@ console.log(street, city);
 
 //CLASSES
 
+//methods are function made within a class
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    return `Hello, my name is ${this.name} and I am ${this.age}`;
+  }
+}
+
+const personOne = new Person("John", 33);
+const personTwo = new Person("Sarah", 28);
+
+console.log(personTwo.greet());
+
 //SUBCLASSES
 
+class Customer extends Person {
+  constructor(name, age, balance) {
+    super(name, age);
+    this.balance = balance;
+  }
+
+  info() {
+    return `${this.name} owes ${this.balance}`;
+  }
+}
+
+const customer1 = new Customer("kevin", 32, 300);
+
+console.log(customer1.info());
+
 //MODULES
+
+//webpack is a module bundler
+//babel allows you to use ES6 modules in react
+
+//file 1 (file1.js)
+// export const name = "Robbie";
+
+//file 2 (file2.js)
+// import { name } from "./file1";
